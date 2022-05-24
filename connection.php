410ec8893ff984e8ece30e155f4a8787af8659e1
Nhật Anh <91766695+anhpnn@users.noreply.github.com>
2022-05-24 19:59:57 +0700

    edit file connection

--- connection.php
+++ connection.php
@@ -1,7 +1,6 @@
 <?php
 
 $conn = pg_connect("postgres://adsoxlxfioxyeo:3f90afe752c788e830cc12e9253c771a3cce1087ece9ff2729518e2bf77c8456@ec2-54-172-175-251.compute-1.amazonaws.com:5432/deq1q56aprtlfm");
-	echo 'Connected Successfully !!!';
 	if(!$conn)
 	{
 		die("Could not connect to database");
