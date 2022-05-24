f5b581e25334a15c740b622e2a0e7091175defec
Nhật Anh <91766695+anhpnn@users.noreply.github.com>
2022-05-24 19:56:27 +0700

    Create app

--- connection.php
+++ connection.php
@@ -1,11 +1,16 @@
 <?php
-	/*$Connect = mysqli_connect("localhost","root","","qlbh") or die("Lỗi".mysqli_error($Connect));
-	mysqli_query($Connect,'SET NAMES "utf8"');
-	//mysqli_close($Connect);*/
+
+$conn = pg_connect("postgres://adsoxlxfioxyeo:3f90afe752c788e830cc12e9253c771a3cce1087ece9ff2729518e2bf77c8456@ec2-54-172-175-251.compute-1.amazonaws.com:5432/deq1q56aprtlfm");
+	echo 'Connected Successfully !!!';
+	if(!$conn)
+	{
+		die("Could not connect to database");
+    }
+
 	
 	
-$conn = pg_connect("host=localhost port=5432 dbname=ATNshop user=postgres password=20102001");
+/*$conn = pg_connect("host=localhost port=5432 dbname=ATNshop user=postgres password=20102001");
 if (!$conn) {
 	die("Connection failed");
-}
+}*/
 ?>
