625f319ff233125e02414e0de25a6471d3536b58
Nhật Anh <91766695+anhpnn@users.noreply.github.com>
2022-05-26 12:43:19 +0700

    Edit update_product file

--- update_product.php
+++ update_product.php
@@ -232,8 +232,8 @@ echo "<SELECT name ='CategoryList' class='from-control'>
 							copy($pic['tmp_name'], "ATNtoy/".$pic['name']);
 							$filepic = $pic['name'];
 							
-							$sqlString = "UPDATE product set product_name ='$proname', price = '$price', smalldesc ='$short', detaildesc ='$detail', pro_qty ='$qty', pro_image ='$filepic', cat_id ='$cat', branch_name = '$branch', 
-							prodate ='".date('Y-m-d H:i:s')."' where product_id ='$id'";
+							$sqlString = "UPDATE product set product_name ='$proname', price = '$price', smalldesc ='$short', detaildesc ='$detail', pro_qty ='$qty', pro_image ='$filepic', cat_id ='$cat', branch_name = '$branch'
+							 where product_id ='$id'";
 							pg_query($conn,$sqlString);
 							echo '<meta http-equiv="refresh" content="0;URL=?page=pm"';	
 					
@@ -251,8 +251,7 @@ echo "<SELECT name ='CategoryList' class='from-control'>
 			else
 			{
 				
-					$sqlString = "UPDATE product set product_name ='$proname', price = '$price', smalldesc ='$short',  detaildesc ='$detail', pro_qty='$qty', cat_id='$cat',branch_id= '$branch', 
-					prodate='".date('Y-m-d H:i:s')."' where product_id ='$id'";
+					$sqlString = "UPDATE product set product_name ='$proname', price = '$price', smalldesc ='$short',  detaildesc ='$detail', pro_qty='$qty', cat_id='$cat',branch_id= '$branch' where product_id ='$id'";
 					pg_query($conn,$sqlString);
 					echo '<meta http-equiv="refresh" content="0;URL =?page=pm"';	
 					
